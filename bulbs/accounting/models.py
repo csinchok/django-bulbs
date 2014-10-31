de98fc21d6ded0acf23e013c911cf5b90d8d678a
Chris Sinchok <csinchok@theonion.com>
2014-10-31 14:10:41 -0500

    Accounting work

--- bulbs/accounting/models.py
+++ bulbs/accounting/models.py
@@ -1,8 +1,34 @@
+from django.core.cache import cache
 from django.db import models
 
 from bulbs.contributions.models import ContributorRole, Contribution
 from bulbs.content.models import FeatureType
 from django.contrib.contenttypes.models import ContentType
+
+
+class AccountingRuleManager(models.Manager):
+
+    def cached(self):
+        rules = cache.get("accounting-rules")
+        if rules is None:
+            rules = self.objects.values()
+            cache.set("accounting-rules", rules, 60 * 10)
+        return [AccountingRule(**rule) for rule in rules]
+
+    def match(self, contribution):
+        rules = self.cached()
+        for rule in rules:
+            if rule.roles:
+                if contribution.role_id not in rule.roles:
+                    return False
+            if rule.feature_types:
+                if contribution.content.feature_type_id not in rule.feature_types:
+                    return False
+            if rule.content_types:
+                content_type = ContentType.objects.get_for_model(contribution.content)
+                if content_type.id not in rule.content_types:
+                    return False
+        return True
 
 
 class AccountingRule(models.Model):
--- bulbs/accounting/views.py
+++ bulbs/accounting/views.py
@@ -1,11 +1,16 @@
 """API Views and ViewSets"""
 
+import datetime
+
+from django.utils import timezone, dateparse
+
 from rest_framework import viewsets, routers
 
 from bulbs.api.permissions import IsAdminUser
 from bulbs.api.mixins import UncachedResponse
+from bulbs.content.models import Content
 
-from .models import AccountingRule
+from .models import AccountingRule, AccountingOverride
 from .serializers import AccountingRuleSerializer
 
 
@@ -14,6 +19,42 @@ class AccountingRuleViewSet(UncachedResponse, viewsets.ModelViewSet):
     serializer_class = AccountingRuleSerializer
 
     permission_classes = [IsAdminUser]
+
+
+class AccountingReportViewSet(UncachedResponse, viewsets.GenericViewSet):
+
+    def list(self, request, *args, **kwargs):
+        now = timezone.now()
+
+        start_date = datetime.datetime(
+            year=now.year,
+            month=now.month,
+            day=1,
+            tzinfo=now.tzinfo)
+        if "start" in self.request.GET:
+            start_date = dateparse.parse_date(self.request.GET["start"])
+
+        end_date = now
+        if "end" in self.request.GET:
+            end_date = dateparse.parse_date(self.request.GET["end"])
+
+        content = Content.objects.filter(published__range=(start_date, end_date))
+        content_ids = content.values_list("pk", flat=True)
+
+        rules = AccountingRule.objects.all()
+        contributions = Contribution.objects.filter(content__in=content_ids).select_related("content")
+        contribution_ids = contributions.values_list("pk", flat=True)
+        overrides = AccountingOverride.objects.filter(contribution__in=contribution_ids)
+
+        data = []
+        for contribution in contributions:
+            for rule in rules:
+                if rule.matches(content)
+            data.append({
+                "author": contribution.contributor,
+                "content": contribution.content,
+                "amount": 
+            })
 
 
 api_v1_router = routers.DefaultRouter()
