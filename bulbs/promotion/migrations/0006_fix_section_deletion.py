f6847735fbd8b85c8e9c927fa26e09c08bcb56dd
Michael Butkovic <michael.butkovic@gmail.com>
2016-04-21 16:15:38 -0500

    empty migration

---
+++ bulbs/promotion/migrations/0006_fix_section_deletion.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('promotion', '0005_auto_20150528_1434'),
+    ]
+
+    operations = [
+    ]
